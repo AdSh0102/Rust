414d4bcd2b2784ec1f11fc54b5de081febff1d4d
AdSh0102 <adityasharan2001@gmail.com>
2024-02-02 22:24:26 +0530

    more ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     let m1 = String::from("Hello");
     let m2 = String::from("world");
-    let (m1, m2) = greet(m1, m2);
+    // (m1, m2) = 
+    greet(&m1, &m2);
     let s = format!("{} {}", m1, m2);
-    println!("{}", s);
+    println!("{}",s);
 }
 
-fn greet(g1: String, g2: String) -> (String, String) {
+fn greet(g1: &String, g2: &String) {
     println!("{} {}!", g1, g2);
-    (g1, g2)
+    // (g1, g2)
 }
