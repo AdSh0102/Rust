9a11fca5677a7a9950d7ad00178bfbfe8576ce68
AdSh0102 <adityasharan2001@gmail.com>
2024-03-02 00:17:02 +0530

    gitignore update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,13 +1,6 @@
 fn main() {
-    let m1 = String::from("Hello");
-    let m2 = String::from("world");
-    // (m1, m2) = 
-    greet(&m1, &m2);
-    let s = format!("{} {}", m1, m2);
-    println!("{}",s);
-}
-
-fn greet(g1: &String, g2: &String) {
-    println!("{} {}!", g1, g2);
-    // (g1, g2)
-}
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    let mut num: &i32 = &v[2];
+    // v.push(4);
+    println!("Third element is {}", *num);
+    }
