fd433efd37b991b7516643f0d69283cb59c9b3ab
AdSh0102 <adityasharan2001@gmail.com>
2024-11-13 00:08:20 +0530

    ownership move

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut v: Vec<i32> = vec![1, 2, 3];
-    let mut num: &i32 = &v[2];
-    // v.push(4);
-    println!("Third element is {}", *num);
-    }
+    // let mut v: Vec<i32> = vec![1, 2, 3];
+    // let mut num: &i32 = &v[2];
+    // // v.push(4);
+    // println!("Third element is {}", *num);
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    println!("{s1}, world!");
+
+}
