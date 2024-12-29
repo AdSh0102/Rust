b50680bb55cb38c9a060668befa3a52897c2f77c
AdSh0102 <adityasharan2001@gmail.com>
2024-12-29 17:26:46 +0530

    structs and methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,15 +1,31 @@
-#[derive(Debug)]
-struct Rectangle {
+struct Rectangle{
     width: u32,
     height: u32,
 }
 
+impl Rectangle{
+    fn area(&self) -> u32 {
+        return self.height * self.width;
+    }
+    fn can_hold(&self, rect1: &Rectangle) -> bool {
+        self.height > rect1.height && self.width > rect1.width
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    dbg!(&rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
