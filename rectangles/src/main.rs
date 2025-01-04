61060406ee79fb2a32935b6b869ce0184862910d
AdSh0102 <adityasharan2001@gmail.com>
2025-01-04 15:04:25 +0530

    struct functions without self argument

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,6 +10,12 @@ impl Rectangle{
     fn can_hold(&self, rect1: &Rectangle) -> bool {
         self.height > rect1.height && self.width > rect1.width
     }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -17,15 +23,19 @@ fn main() {
         width: 30,
         height: 50,
     };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
+    // let rect2 = Rectangle {
+    //     width: 10,
+    //     height: 40,
+    // };
+    // let rect3 = Rectangle {
+    //     width: 60,
+    //     height: 45,
+    // };
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    // println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    
+    let sq = Rectangle::square(50);
+    
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&sq));
+
 }
